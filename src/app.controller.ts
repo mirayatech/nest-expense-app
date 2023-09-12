1edd72c6b2302cdf65fdc3ba33dfb327db0028b0
〽️iraya <mirayaabrodi@gmail.com>
2023-09-12 07:59:55 +0200

    feat: Add route for creating income reports

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,15 @@
-import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Body,
+} from '@nestjs/common';
 import { data } from './data';
 import { ReportTypeEnum } from './type';
-
+import { v4 as uuid } from 'uuid';
 @Controller('report/:type')
 export class AppController {
   @Get()
@@ -20,8 +28,20 @@ export class AppController {
   }
 
   @Post('')
-  createIncomeReport() {
-    return 'Created income report';
+  createIncomeReport(
+    @Body() body: { amount: number; source: string },
+    @Param('type') type: string,
+  ) {
+    const newReport = {
+      id: uuid(),
+      source: body.source,
+      amount: body.amount,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: type === 'income' ? ReportTypeEnum.INCOME : ReportTypeEnum.EXPENSE,
+    };
+    data.report.push(newReport);
+    return newReport;
   }
 
   @Put(':id')
