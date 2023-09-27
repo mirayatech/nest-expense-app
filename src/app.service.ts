799901c396ed0b6b03f9bf94383fa7dcdd85614a
〽️iraya <mirayaabrodi@gmail.com>
2023-09-27 05:59:00 +0200

    feat: Create AppService with CRUD Operations

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,60 @@
 import { Injectable } from '@nestjs/common';
+import { ReportTypeEnum, data } from './data';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getAllReports(type: ReportTypeEnum) {
+    return data.report.filter((report) => report.type === type);
+  }
+
+  getReportById(type: ReportTypeEnum, id: string) {
+    return data.report
+      .filter((report) => report.type === type)
+      .find((report) => report.id === id);
+  }
+
+  cretaeReport(type: ReportTypeEnum, body: { amount: number; source: string }) {
+    const newReport = {
+      id: uuid(),
+      source: body.source,
+      amount: body.amount,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type,
+    };
+    data.report.push(newReport);
+    return newReport;
+  }
+
+  updateReport(
+    type: ReportTypeEnum,
+    id: string,
+    body: { amount: number; source: string },
+  ) {
+    const reportToUpdate = data.report
+      .filter((report) => report.type === type)
+      .find((report) => report.id === id);
+
+    if (!reportToUpdate) return;
+
+    const reportIndex = data.report.findIndex(
+      (report) => report.id === reportToUpdate.id,
+    );
+
+    data.report[reportIndex] = {
+      ...data.report[reportIndex],
+      ...body,
+      updated_at: new Date(),
+    };
+
+    return data.report[reportIndex];
+  }
+
+  deleteReport(id: string) {
+    const reportIndex = data.report.findIndex((report) => report.id === id);
+
+    if (reportIndex === -1) return;
+    data.report.splice(reportIndex, 1);
   }
 }
