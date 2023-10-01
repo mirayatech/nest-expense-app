0df9c614d2d31dfe8078f66863f014bfac87de35
〽️iraya <mirayaabrodi@gmail.com>
2023-10-01 17:24:40 +0200

    feat: create dto file for update, create and all reports

---
+++ src/dtos/report.dto.ts
@@ -0,0 +1,46 @@
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+} from 'class-validator';
+import { Exclude } from 'class-transformer';
+import { ReportTypeEnum } from 'src/data';
+
+export class CreateReportDto {
+  @IsNumber()
+  @IsPositive()
+  amount: number;
+
+  @IsString()
+  @IsNotEmpty()
+  source: string;
+}
+
+export class UpdateReportDto {
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  amount: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  source: string;
+}
+
+export class ReportResponseDto {
+  id: string;
+  source: string;
+  amount: number;
+  created_at: Date;
+
+  @Exclude()
+  updated_at: Date;
+  type: ReportTypeEnum;
+
+  constructor(partial: Partial<ReportResponseDto>) {
+    Object.assign(this, partial);
+  }
+}
